63556051cb7f069db563ff3e68f3a132c3439617
eciran <55311291+eciran@users.noreply.github.com>
2020-04-27 00:20:07 +0300

    Layout-Partial düzenlemeleri

--- UserPageDeneme/Controllers/HomeController.cs
+++ UserPageDeneme/Controllers/HomeController.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace UserPageDeneme.Controllers
 {
-    public class HomeController : Controller
-    {
-        // GET: Home
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public ActionResult Index()
+		{
+			return View();
+		}
+		public ActionResult Login()
+		{
+			return View();
+		}
+		public ActionResult Product()
+		{
+			return  View();
+		}
+		public ActionResult ContactForm()
+		{
+			return View();
+		}
+	}
 }
